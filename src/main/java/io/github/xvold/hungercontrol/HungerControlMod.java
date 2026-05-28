0731e4f0256b1fc82fad04360f643e274d94f562
xvolD <xvolD@users.noreply.github.com>
2026-05-29 06:25:27 +0700

    1.20.1 Fabric

--- src/main/java/io/github/xvold/hungercontrol/HungerControlMod.java
+++ src/main/java/io/github/xvold/hungercontrol/HungerControlMod.java
@@ -1,42 +1,21 @@
 package io.github.xvold.hungercontrol;
 
-import com.mojang.logging.LogUtils;
 import io.github.xvold.hungercontrol.command.HungerControlCommand;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Mod(HungerControlMod.MODID)
-public class HungerControlMod {
+public class HungerControlMod implements ModInitializer {
     public static final String MODID = "hungercontrol";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
-    public HungerControlMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::commonSetup);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-        MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
+    @Override
+    public void onInitialize() {
+        Config.load();
         LOGGER.info("Hunger Control initialized. Multiplier: {}", Config.exhaustionMultiplier);
-    }
-
-    private void onRegisterCommands(final RegisterCommandsEvent event) {
-        HungerControlCommand.register(event.getDispatcher());
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        LOGGER.info("Hunger Control active on server.");
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            HungerControlCommand.register(dispatcher);
+        });
     }
 }
